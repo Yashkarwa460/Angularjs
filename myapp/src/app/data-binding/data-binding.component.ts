a58f2d434049df9d4602a856dcf2ea63206f126c
yashkarwa460 <yashkarwa460@gmail.com>
2019-01-13 12:55:51 +0530

    button-changing-alert-property

--- myapp/src/app/data-binding/data-binding.component.ts
+++ myapp/src/app/data-binding/data-binding.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { $ } from 'protractor';
+import { flattenStyles } from '@angular/platform-browser/src/dom/dom_renderer';
 
 @Component({
   selector: 'app-data-binding',
@@ -12,6 +13,38 @@ institute = 'welcome to codekul'
   constructor() { }
   myClick(evDt : any ) {
     console.log(evDt)
+  }
+  alSts = {
+    iswarning: false,
+    issuccess: false,
+    isinfo: false,
+    isdanger: false
+  }
+  myAl(wh : string){
+    if(wh === 'warning'){
+      this.alSts.iswarning = true
+      this.alSts.issuccess = false
+      this.alSts.isdanger = false
+      this.alSts.isinfo = false
+    } else if(wh === 'success'){
+
+      this.alSts.iswarning = false
+      this.alSts.issuccess = true
+      this.alSts.isdanger = false
+      this.alSts.isinfo = false
+    } else if(wh === 'danger'){
+
+      this.alSts.iswarning = false
+      this.alSts.issuccess = false
+      this.alSts.isdanger = true
+      this.alSts.isinfo = false
+    } else{
+      
+      this.alSts.iswarning = false
+      this.alSts.issuccess = false
+      this.alSts.isdanger = false
+      this.alSts.isinfo = true
+    }
   }
   ngOnInit() {
   }
