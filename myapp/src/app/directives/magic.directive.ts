fc50f3bb344146cdcda728946f829fd14e7d0133
yashkarwa460 <yashkarwa460@gmail.com>
2019-02-09 12:40:49 +0530

    magicapp directive color change of boxes

--- myapp/src/app/directives/magic.directive.ts
+++ myapp/src/app/directives/magic.directive.ts
@@ -1,10 +1,35 @@
-import { Directive } from '@angular/core';
+import { Directive, OnInit,  HostBinding, HostListener, Input} from '@angular/core';
 
 @Directive({
   selector: '[appMagic]'
 })
-export class MagicDirective {
+export class MagicDirective implements OnInit {
+
+  @HostBinding('style.background')
+  bkCl: string
+
+  @Input()
+  dfCl : string
 
   constructor() { }
+
+  ngOnInit() {
+    this.bkCl = this.dfCl
+  }
+
+  magicColor() {
+    this.bkCl =  `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)} )`
+    console.log(this.bkCl)
+  }
+
+  @HostListener('mouseenter')
+  msIn() {
+    this.magicColor()
+  }
+
+  @HostListener('mouseleave')
+  msLv() {
+    this.magicColor()
+  }
 
 }
